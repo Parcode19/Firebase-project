6c8adb31c7dc53eb799d8bfa6624f6de7c2016c1
Parth <parthp15@yahoo.com>
2023-08-28 12:37:52 -0700

    wepack loaded

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("// For Firebase JS SDK v7.20.0 and later, measurementId is optional\nconst firebaseConfig = {\n  apiKey: \"AIzaSyBkc630N7EVWy3dCn0bxYQa6_9LXr-oT-Y\",\n  authDomain: \"first-project-4dd61.firebaseapp.com\",\n  projectId: \"first-project-4dd61\",\n  storageBucket: \"first-project-4dd61.appspot.com\",\n  messagingSenderId: \"342580336958\",\n  appId: \"1:342580336958:web:c7c3d9add8ac163ea0746f\",\n  measurementId: \"G-KMX5GGTB0P\"\n};\n\n//# sourceURL=webpack://firebase-project/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkc630N7EVWy3dCn0bxYQa6_9LXr-oT-Y",
+  authDomain: "first-project-4dd61.firebaseapp.com",
+  projectId: "first-project-4dd61",
+  storageBucket: "first-project-4dd61.appspot.com",
+  messagingSenderId: "342580336958",
+  appId: "1:342580336958:web:c7c3d9add8ac163ea0746f",
+  measurementId: "G-KMX5GGTB0P"
+};
