80dbce1c38a105f06b755e0cf58e4d2466150a6d
Parth <parthp15@yahoo.com>
2023-08-28 17:53:11 -0700

    fetching data from firestore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBkc630N7EVWy3dCn0bxYQa6_9LXr-oT-Y",
@@ -8,3 +11,27 @@ const firebaseConfig = {
   appId: "1:342580336958:web:c7c3d9add8ac163ea0746f",
   measurementId: "G-KMX5GGTB0P"
 };
+
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+  .then ((snapshot) => {
+    let books =[];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books);
+  })
+
+  .catch (err => {
+    console.log(err.message)
+  })
